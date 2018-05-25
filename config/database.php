fce70b3323ec3cbb02b0d20b3a9435f525e44496
Alan Cornejo <alancornejo@icloud.com>
2018-05-25 11:55:17 -0500

    Agregando el puerto para las conexiones de MYSQL

--- config/database.php
+++ config/database.php
@@ -54,6 +54,7 @@ return [
         'mysql' => [
             'driver'    => env('LARAVEL_DRIVER', 'sqlite'),
             'host'      => env('LARAVEL_HOST', 'localhost'),
+            'port'      => env('LARAVEL_PORT', '3306'),
             'database'  => env('LARAVEL_DATABASE', 'forge'),
             'username'  => env('LARAVEL_USERNAME', 'forge'),
             'password'  => env('LARAVEL_PASSWORD', ''),
@@ -65,6 +66,7 @@ return [
         'laravel' => [
             'driver'    => env('LARAVEL_DRIVER', 'sqlite'),
             'host'      => env('LARAVEL_HOST', 'localhost'),
+            'port'      => env('LARAVEL_PORT', '3306'),
             'database'  => env('LARAVEL_DATABASE', 'forge'),
             'username'  => env('LARAVEL_USERNAME', 'forge'),
             'password'  => env('LARAVEL_PASSWORD', ''),
@@ -76,6 +78,7 @@ return [
         'sapia' => [
             'driver'    => env('SAPIA_DRIVER', 'sqlite'),
             'host'      => env('SAPIA_HOST', 'localhost'),
+            'port'      => env('SAPIA_PORT', '3306'),
             'database'  => env('SAPIA_DATABASE', 'forge'),
             'username'  => env('SAPIA_USERNAME', 'forge'),
             'password'  => env('SAPIA_PASSWORD', ''),
