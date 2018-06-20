4af77508774da2299cddfceb81d09f3821bb1156
Alex Christian <aquispe.developer@gmail.com>
2018-06-20 17:02:33 -0500

    Modificado modelo para crear Cola

--- app/Models/QueuesTemplate.php
+++ app/Models/QueuesTemplate.php
@@ -12,10 +12,11 @@ class QueuesTemplate extends Model
     public    $timestamps   = false;
 
     protected $fillable = [
-        'id', 'name_template', 'music_onhold', 'empty_template', 'timeout_template', 'memberdelay_template', 'ringinuse_template', 'autopause_template', 'autopausebusy_template', 'wrapuptime_template', 'maxlen_template', 'estado_id',
+        'name_template', 'music_onhold', 'empty_template', 'timeout_template', 'memberdelay_template', 'ringinuse_template', 'autopause_template', 'autopausebusy_template', 'wrapuptime_template', 'maxlen_template', 'estado_id',
     ];
 
     public function musicOnHold(){
         return $this->belongsTo('Cosapi\Models\QueuesMusicOnHold','music_onhold');
     }
+
 }
